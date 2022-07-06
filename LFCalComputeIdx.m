93afb028f06525fed3c0a1d28eea4f3ed78010c8
wookiem8 <wzha0123@student.monash.edu>
2022-07-07 03:10:59 +1000

    Added error checking

--- LFCalComputeIdx.m
+++ LFCalComputeIdx.m
@@ -1,7 +1,9 @@
 function [InterpIdx] = LFCalComputeIdx(LFSize, IdxSize, CalInfo, RectOptions)
+    assert(length(IdxSize) == 4, 'Incorrect dimensions for IdxSize. Should be a 4 length row vector.')
+
     RectOptions = LFDefaultField('RectOptions', 'RectCamIntrinsicsH', LFDefaultIntrinsics(LFSize, CalInfo));
     RectOptions = LFDefaultField('RectOptions', 'Precision', 'single');
-    
+
     t_in = cast(IdxSize{1}, 'uint16');
     s_in = cast(IdxSize{2}, 'uint16');
     v_in = cast(IdxSize{3}, 'uint16');
